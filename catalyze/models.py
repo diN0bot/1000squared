e0bb0bb23a374ec642d5b603c8b87461613edcd7
lucy <lucy.mendel@rackspace.com>
2016-08-29 03:17:56 -0700

    add full path to image upload path

--- catalyze/models.py
+++ catalyze/models.py
@@ -1,9 +1,12 @@
 from django import forms
 from django.db import models
+import os
+from settings import BASE_DIR
 
 
 class CatalystPic(models.Model):
-    pic = models.ImageField(upload_to='uploaded_pics/')
+    upload_to = os.path.join(BASE_DIR, 'uploaded_pics/')
+    pic = models.ImageField(upload_to=upload_to)
 
 class CatalystPicForm(forms.ModelForm):
     class Meta:
